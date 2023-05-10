409b15000e8be66c1854e66611ff30b084673437
khd325 <hdk325@naver.com>
2023-05-10 17:44:30 +0900

    인터페이스 기반 프록시와 클래스 기반 프록시

--- proxy/src/main/java/hello/proxy/app/v2/OrderRepositoryV2.java
+++ proxy/src/main/java/hello/proxy/app/v2/OrderRepositoryV2.java
@@ -10,7 +10,7 @@ public class OrderRepositoryV2 {
 
     private void sleep(int millis) {
         try {
-            Thread.sleep(1000);
+            Thread.sleep(millis);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
