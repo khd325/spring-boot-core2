3c8754b512b3df986431f5095d83b6c98a358aac
khd325 <hdk325@naver.com>
2023-05-24 22:37:40 +0900

    스프링 AOP 포인트컷

--- aop/src/test/java/hello/aop/pointcut/ExecutionTest.java
+++ aop/src/test/java/hello/aop/pointcut/ExecutionTest.java
@@ -2,11 +2,14 @@ package hello.aop.pointcut;
 
 import hello.aop.member.MemberServiceImpl;
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.aspectj.AspectJExpressionPointcut;
 
 import java.lang.reflect.Method;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
 public class ExecutionTest {
@@ -24,4 +27,74 @@ public class ExecutionTest {
     public void printMethod() {
         log.info("helloMethod={}", helloMethod);
     }
+
+    @Test
+        public void exactMatch() {
+            //public java.lang.String hello.aop.member.MemberServiceImpl.hello(java.lang.String)
+            pointcut.setExpression("execution(public String hello.aop.member.MemberServiceImpl.hello(String))");
+            assertThat(pointcut.matches(helloMethod,MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    public void allMatch() {
+        pointcut.setExpression("execution(* *(..))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    public void nameMatch() {
+        pointcut.setExpression("execution(* hello(..))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    public void nameMatch1() {
+        pointcut.setExpression("execution(* hel*(..))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    public void nameMatch2() {
+        pointcut.setExpression("execution(* *el*(..))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    public void nameMatchFalse() {
+        pointcut.setExpression("execution(* nono(..))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isFalse();
+    }
+
+    @Test
+    public void packageExactMatch1() {
+        pointcut.setExpression("execution(* hello.aop.member.MemberServiceImpl.hello(..))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    public void packageExactMatch2() {
+        pointcut.setExpression("execution(* hello.aop.member.*.*(..))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    public void packageExactMatchFalse() {
+        pointcut.setExpression("execution(* hello.aop.*.*(..))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isFalse();
+    }
+
+    @Test
+    public void packageMatchSubPackage1() {
+        pointcut.setExpression("execution(* hello.aop..member..*(..))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    public void packageMatchSubPackage2() {
+        pointcut.setExpression("execution(* hello.aop..*.*(..))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+
+
 }
--- aop/summary/스프링 AOP - 포인트컷.md
+++ aop/summary/스프링 AOP - 포인트컷.md
@@ -16,3 +16,35 @@
 - `@annotation`: 메서드가 주어진 애노테이션을 가지고 있는 조인 포인트 매칭
 - `@args`: 전달된 실제 인수의 런타임 타입이 주어진 타입의 애노테이션을 갖는 조인 포인트
 - `bean`: 스프링 전용 포인트컷 지시자. 빈의 이름으로 포인트컷 지정
+
+## execution
+
+```text
+execution(modifiers-pattern? ret-type-pattern declaring-type-pattern?namepattern(param-pattern) throws-pattern?)
+
+execution(접근제어자? 반환타입 선언타입?메서드이름(파라미터) 예외?)
+```
+
+메서드 실행 조인 포인트 매칭
+
+?는 생략 가능
+
+'*' 같은 패턴을 지정할 수 있다.
+
+```java
+@Test
+public void exactMatch(){
+        //public java.lang.String hello.aop.member.MemberServiceImpl.hello(java.lang.String)
+        pointcut.setExpression("execution(public String hello.aop.member.MemberServiceImpl.hello(String))");
+        assertThat(pointcut.matches(helloMethod,MemberServiceImpl.class)).isTrue();
+        }
+```
+
+**매칭 조건**
+
+- 접근제어자?: public
+- 반환타입: String
+- 선언타입?: hello.aop.member.MemberServiceImpl
+- 메서드이름: hello
+- 파라미터: (String)
+- 예외?: 생략
